d12f3c83ca98b8406190afdc662983d8bb0ec6ad
Håvard Hunstadbråten <hhunstadbraten@ndlocis.mil.no>
2023-12-04 15:49:33 +0100

    sdfsdf

--- AdentOfCodeNunit/Day4Tests.cs
+++ AdentOfCodeNunit/Day4Tests.cs
@@ -57,6 +57,6 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11");
 
         var logic = new AdventOfCodeLogic.Day4();
         var result = await logic.SolvePart2(dataProvider);
-        Assert.That(result, Is.EqualTo(23678));
+        Assert.That(result, Is.EqualTo(15455663));
     }
 }
